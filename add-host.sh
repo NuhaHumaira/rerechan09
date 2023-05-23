972389019ea5b425c43fb9d1331e4385f2514039
Rerechan02 <122332019+Rerechan02@users.noreply.github.com>
2023-05-23 22:29:31 +0700

    Add files via upload

---
+++ add-host.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 $
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
+
+clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -p " Input : " domain
+echo "$domain" > /etc/xray/domain
+echo "$domain" > /etc/v2ray/domain
+echo ""
+if [ -z $domain ]; then
+echo "SUCCSES CHANGE DOMAIN"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+else
+echo "IP=$domain" > /var/lib/SIJA/ipvps.conf
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "Dont forget to renew cert"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+fi
